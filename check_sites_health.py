51d380a9f922538e4deb9a5beb8885f0feb38afc
Jack <mtjeck@gmail.com>
2016-11-25 00:04:33 +0300

    reduce code in functions. add print-function. add urls storage-lists

--- check_sites_health.py
+++ check_sites_health.py
@@ -14,9 +14,7 @@ def load_urls4check(path):
 
 def is_server_respond_with_200(url):
     try:
-        if requests.get(url).status_code == 200:
-            return True
-        return False
+        return requests.get(url).status_code == 200
     except:
         return False
 
@@ -31,23 +29,32 @@ def get_domain_expiration_date(domain_name):
 def is_available_month_later(domain_name):
     month_later = datetime.date.today() + relativedelta(months=+1)
     expired_date = get_domain_expiration_date(domain_name).date()
-    if expired_date < month_later:
-        return False
-    else:
-        return True
+    return expired_date > month_later
 
+
+def print_urls(url_list):
+    if url_list:
+        for i, url in enumerate(url_list):
+            print("\t%s) %s" % (i+1, url))
 
 if __name__ == '__main__':
     urls = load_urls4check(input("Please enter path to urls-file: "))
     if urls:
-        for url in urls:
-            if is_server_respond_with_200(url):
-                print('%s is available.' % url)
-                if is_available_month_later:
-                    print('Domain name of %s is payed more than 1 month in advance.' % url)
-                else:
-                    print('Domain name of %s will be blocked in less than a month.' % url)
-            else:
-                print('%s is NOT available!' % url)
+        available_urls = [url for url in urls if is_server_respond_with_200(url)]
+        unavailable_urls = [url for url in set(urls).difference(available_urls)]
+        payed_domains = [url for url in available_urls if is_available_month_later(url)]
+        expired_domains = [url for url in set(available_urls).difference(payed_domains)]
+        if available_urls:
+            print("Available urls:")
+            print_urls(available_urls)
+        if payed_domains:
+            print("These domains are payed more than 1 month in advance:")
+            print_urls(payed_domains)
+        if expired_domains:
+            print("These domains ain\'t payed more than 1 month in advance or has been expired:")
+            print_urls(expired_domains)
+        if unavailable_urls:
+            print("Unavailable urls:")
+            print_urls(unavailable_urls)
     else:
         print("Incorrect path to urls-file.")
